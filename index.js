c642de4f1f1fde6b0c2796033cc3595ada6b35dc
Cahya Isra <cahya@wvi.co.id>
2020-07-12 16:48:00 +0700

    add contact

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express =require('express')
 const app = express()
-const fs = require('fs');
 const { Client, MessageMedia } = require('whatsapp-web.js');
 const PORT = 3012 || process.env.PORT;
-const SESSION_FILE_PATH = './session.json';
+// const SESSION_FILE_PATH = './session.json';
 const multer  = require('multer')
 var storage = multer.memoryStorage()
 var upload = multer({ storage: storage })
@@ -22,7 +21,7 @@ app.get('/setup', async(req, res) => {
 })
 
 app.get('/', (req, res) => {
-    res.redirect('/state')
+    res.redirect('/send')
 })
 
 app.get('/reinit', async (req, res) => {
@@ -30,7 +29,9 @@ app.get('/reinit', async (req, res) => {
     try{
         await client.destroy()
         client.initialize()
+        state.status = 're-init'
     } catch(err){
+        console.log(err)
         message = 'Re-Init Failed'
     }
     res.json({state, message})
@@ -56,15 +57,15 @@ app.get('/reset', async (req, res) => {
     res.json({state, message})
 })
 
-app.get('/destroy-session', (req, res) => {
-    try {
-        fs.unlinkSync(SESSION_FILE_PATH)
-        res.json({msg: 'Session Purge'})
-      } catch(err) {
-        console.error(err)
-        res.json({msg: 'Purge Failed'})
-      }
-})
+// app.get('/destroy-session', (req, res) => {
+//     try {
+//         fs.unlinkSync(SESSION_FILE_PATH)
+//         res.json({msg: 'Session Purge'})
+//       } catch(err) {
+//         console.error(err)
+//         res.json({msg: 'Purge Failed'})
+//       }
+// })
 
 app.get('/state', (req, res)=>{
     res.json({state})
@@ -86,47 +87,61 @@ app.get('/contacts', async(req, res) => {
 })
 
 app.get('/send', async(req, res) => {
-    res.render('form')
-})
-
-app.post('/send', upload.single('image'), async(req, res) => {
+    if(state.status === 'qr' ){
+        res.redirect('/setup')
+    }
     try{
-        let {ids, message} = req.body
-        if(!ids && !message){
-            return res.status(404).end()
-        }
-        let results = []
-        if(!Array.isArray(ids)){
-            ids = ids.trim().split('|')
-        }
-        let content = message
-        
-        if(req.file && message === 'image'){
-            let mimetype = req.file.mimetype
-            let base64Img = req.file.buffer.toString('base64')
-            content = new MessageMedia(mimetype, base64Img)
-        }
-
-        for(let id of ids){
-            let data = await client.sendMessage(id, content)
-            results.push(data)
-        }
-        res.json({ids, content, results})
-    } catch(err){
-        console.log(err)
-        res.json({err})
+        let contacts = await client.getContacts()
+        res.render('form', {contacts})
+    } catch (err){
+        res.status(404).end()
     }
 })
 
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
+app.post('/send', upload.single('image'), async(req, res) => {
+    let {ids, message} = req.body
+    if(!ids){
+        return res.status(404).end()
+    }
+    let results = []
+    if(!Array.isArray(ids)){
+        ids = ids.trim().split('|')
+    }
+    let imageContent = null;
+    
+    if(req.file){
+        let mimetype = req.file.mimetype
+        let base64Img = req.file.buffer.toString('base64')
+        imageContent = new MessageMedia(mimetype, base64Img)
+    }
+
+    for(let id of ids){
+        let result = {id, status: 'Failed'}
+        try{
+            if(imageContent){
+                await client.sendMessage(id, imageContent)
+            }
+            if(message){
+                await client.sendMessage(id, message)
+            }
+            result.status = 'Success'
+        } catch(err){
+            result.status = 'Failed'
+        }
+        results.push(result)
+    }
+    res.json({ids, message, results})
+})
+
+// let sessionCfg;
+// if (fs.existsSync(SESSION_FILE_PATH)) {
+//     sessionCfg = require(SESSION_FILE_PATH);
+// }
 
 const client = new Client({ puppeteer: { headless: true, args: [
     '--no-sandbox', 
     '--disable-setuid-sandbox'
-]}, session: sessionCfg });
+]}});
 
 client.initialize()
 
@@ -139,12 +154,12 @@ client.on('qr', (qr) => {
 client.on('authenticated', (session) => {
     console.log('AUTHENTICATED', session);
     state.status = 'authenticated'
-    sessionCfg = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
+    // sessionCfg = session;
+    // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+    //     if (err) {
+    //         console.error(err);
+    //     }
+    // });
 });
 
 client.on('auth_failure', msg => {
